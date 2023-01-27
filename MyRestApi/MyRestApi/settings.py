5137baae7a91e3a58bf6ff975037abd5b61045ab
Raihan Rafi <85237779+RaihanRafi007@users.noreply.github.com>
2023-01-27 22:59:37 +0600

    Update settings.py

--- MyRestApi/MyRestApi/settings.py
+++ MyRestApi/MyRestApi/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-t4(szv7e(=w*7-z!3n+9@m6zbbqqd8(nw6g#h_==3ozm6u)h+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
